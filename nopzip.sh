2e68adc854469e9cb815727ff54cd3c4e4f98286
shahin <shahinurislama@gmail.com>
2024-04-14 21:24:39 +0530

    added new files

---
+++ nopzip.sh
@@ -0,0 +1,28 @@
+pipeline{
+     agent {
+      label 'nop'
+     }
+       stages{
+        stage('scm'){
+            steps{
+               git url: 'https://github.com/dummyrepos/nopCommerceApr24.git',
+            }      branch: 'develop'
+        }
+        stage('build'){
+          steps{
+           // sh 'dotnet publish -o published/ -c Release src/Presentation/Nop.Web/Nop.Web.csproj'
+          dotnetPublish configuration : 'Release',
+                        outputDirectory : 'published',
+                        project : 'src/Presentation/Nop.Web/Nop.Web.csproj'
+          } 
+
+        }
+        post{
+           success{
+            zip zipFile : 'nop.Web.zip'
+            dir : './published'
+            overwrite : 'true'
+           }
+        }
+       }
+}
